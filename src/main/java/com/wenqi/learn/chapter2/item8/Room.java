262fda87f2136d0b934089eab7f62847e062adc9
liangwenqi <liangwenqi@qdama.cn>
2022-01-21 16:03:07 +0800

    c2 item8 & item9

---
+++ src/main/java/com/wenqi/learn/chapter2/item8/Room.java
@@ -0,0 +1,53 @@
+//package com.wenqi.learn.chapter2.item8;
+//
+//import sun.misc.Cleaner;
+//
+///**
+// * An autocloseable class using a cleaner as a safety net
+// *  以下代码基于JDK9
+// *
+// * @author liangwenqi
+// * @date 2022/1/21
+// */
+//public class Room implements AutoCloseable {
+//    private static final Cleaner cleaner = Cleaner.create();
+//
+//    // Resource that requires cleaning. Must not refer to Room!
+//    private static class State implements Runnable {
+//        int numJunkPiles; // Number of junk piles in this room
+//
+//        State(int numJunkPiles) {
+//            this.numJunkPiles = numJunkPiles;
+//        }
+//
+//        // Invoked by close method or cleaner
+//        @Override
+//        public void run() {
+//            System.out.println("Cleaning room");
+//            numJunkPiles = 0;
+//        }
+//    }
+//
+//    // The state of this room, shared with our cleanable
+//    private final State state;
+//    // Our cleanable. Cleans the room when it’s eligible for gc
+//    private final Cleaner.Cleanable cleanable;
+//
+//    public Room(int numJunkPiles) {
+//        state = new State(numJunkPiles);
+//        cleanable = cleaner.register(this, state);
+//    }
+//
+//    @Override
+//    public void close() {
+//        cleanable.clean();
+//    }
+//}
+//
+//class Adult {
+//    public static void main(String[] args) {
+//        try (Room myRoom = new Room(7)) {
+//            System.out.println("Goodbye");
+//        }
+//    }
+//}
---
+++ src/main/java/com/wenqi/learn/chapter2/item9/CloseResource.java
@@ -0,0 +1,109 @@
+package com.wenqi.learn.chapter2.item9;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import static org.springframework.util.StreamUtils.BUFFER_SIZE;
+
+/**
+ * @author liangwenqi
+ * @date 2022/1/21
+ */
+public class CloseResource {
+
+    /**
+     * try-finally - No longer the best way to close resources!
+     *
+     * try 块和 finally 块中的代码都能够抛出异常。
+     * 例如，在 firstLineOfFile 方法中，由于底层物理设备发生故障，对 readLine 的调用可能会抛出异常，而关闭的调用也可能出于同样的原因而失败。
+     * 在这种情况下，第二个异常将完全覆盖第一个异常。异常堆栈跟踪中没有第一个异常的记录，这可能会使实际系统中的调试变得非常复杂
+     *
+     * @param path
+     * @return
+     * @throws IOException
+     */
+    static String firstLineOfFile(String path) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        try {
+            return br.readLine();
+        } finally {
+            br.close();
+        }
+    }
+
+    /**
+     * try-finally is ugly when used with more than one resource!
+     * @param src
+     * @param dst
+     * @throws IOException
+     */
+    static void copy(String src, String dst) throws IOException {
+        InputStream in = new FileInputStream(src);
+        try {
+            OutputStream out = new FileOutputStream(dst);
+            try {
+                byte[] buf = new byte[BUFFER_SIZE];
+                int n;
+                while ((n = in.read(buf)) >= 0) {
+                    out.write(buf, 0, n);
+                }
+            } finally {
+                out.close();
+            }
+        }
+        finally {
+            in.close();
+        }
+    }
+
+    /**
+     * try-with-resources - the the best way to close resources!
+     * @param path
+     * @return
+     * @throws IOException
+     */
+    static String firstLineOfFileBetter(String path) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            return br.readLine();
+        }
+    }
+
+    /**
+     * try-with-resources on multiple resources - short and sweet
+     * 多资源的关闭流
+     *
+     * @param src
+     * @param dst
+     * @throws IOException
+     */
+    static void copyBetter(String src, String dst) throws IOException {
+        try (InputStream in = new FileInputStream(src);OutputStream out = new FileOutputStream(dst)) {
+            byte[] buf = new byte[BUFFER_SIZE];
+            int n;
+            while ((n = in.read(buf)) >= 0) {
+                out.write(buf, 0, n);
+            }
+        }
+    }
+
+    /**
+     * try-with-resources with a catch clause
+     * 不抛出异常, 返回一个默认值
+     *
+     * @param path
+     * @param defaultVal
+     * @return
+     */
+    static String firstLineOfFile(String path, String defaultVal) {
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            return br.readLine();
+        } catch (IOException e) {
+            return defaultVal;
+        }
+    }
+}
